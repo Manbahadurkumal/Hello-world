8362309117e0108c88da59ad7a7de3b835884ebd
Manbahadurkumal <Mk9307888@gmail.com>
2024-02-27 20:09:49 +0545

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <ul>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Skills</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
+// rafce
